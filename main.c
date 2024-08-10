6427fb6eba3b5d4a2d7a108f51853211a52eeabf
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-10 14:04:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,29 @@
 #include "contatos.h"
 
 int main() {
-    printf("Agenda de Contatos - Bem-vindo!\n");
-    adicionarContato();
+    int opcao;
+    do {
+        printf("\nAgenda de Contatos\n");
+        printf("1. Adicionar Contato\n");
+        printf("2. Listar Contatos\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+                adicionarContato();
+                break;
+            case 2:
+                listarContatos();
+                break;
+            case 3:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    } while(opcao != 3);
+
     return 0;
 }
