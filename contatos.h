f36154dabdeea0375e933baf563998fd9abf985d
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-10 19:12:29 -0300

    Update contatos.h

--- contatos.h
+++ contatos.h
@@ -1,8 +1,17 @@
 #ifndef CONTATOS_H
 #define CONTATOS_H
 
-void adicionarContato();
-void listarContatos();
-void removerContato();
+#define MAX_CONTATOS 100
+#define TAM_NOME 50
+
+typedef struct {
+    char nome[TAM_NOME];
+    char telefone[TAM_NOME];
+} Contato;
+
+void adicionarContato(Contato contatos[], int *total);
+void listarContatos(const Contato contatos[], int total);
+void removerContato(Contato contatos[], int *total);
+void limparTela();
 
 #endif
