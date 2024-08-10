814fa22574211e4d4bf8d31efa5c7af341025be2
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-10 14:04:12 -0300

    Update contatos.c

--- contatos.c
+++ contatos.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include "contatos.h"
 
+#define MAX_CONTATOS 100
+
+char listaDeContatos[MAX_CONTATOS][50];
+int totalContatos = 0;
+
 void adicionarContato() {
-    char nome[50];
-    printf("Digite o nome do contato: ");
-    scanf("%s", nome);
-    printf("Contato %s adicionado com sucesso!\n", nome);
+    if (totalContatos < MAX_CONTATOS) {
+        printf("Digite o nome do contato: ");
+        scanf("%s", listaDeContatos[totalContatos]);
+        totalContatos++;
+        printf("Contato adicionado com sucesso!\n");
+    } else {
+        printf("Agenda cheia!\n");
+    }
+}
+
+void listarContatos() {
+    printf("Lista de Contatos:\n");
+    for (int i = 0; i < totalContatos; i++) {
+        printf("%d. %s\n", i + 1, listaDeContatos[i]);
+    }
 }
