f711b3cd4e03462764c3a20d60e3ec7fa1361293
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-10 19:12:17 -0300

    Update contatos.c

--- contatos.c
+++ contatos.c
@@ -1,42 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "contatos.h"
 
-#define MAX_CONTATOS 100
+void limparTela() {
+    #ifdef _WIN32
+        system("cls");  
+    #else
+        system("clear"); 
+    #endif
+}
 
-char listaDeContatos[MAX_CONTATOS][50];
-int totalContatos = 0;
-
-void adicionarContato() {
-    if (totalContatos < MAX_CONTATOS) {
-        printf("Digite o nome do contato: ");
-        scanf("%s", listaDeContatos[totalContatos]);
-        totalContatos++;
-        printf("Contato adicionado com sucesso!\n");
-    } else {
+void adicionarContato(Contato contatos[], int *total) {
+    if (*total >= MAX_CONTATOS) {
         printf("Agenda cheia!\n");
+        return;
     }
+
+    limparTela();
+    printf("Adicionar Contato\n");
+    printf("Nome: ");
+    fgets(contatos[*total].nome, TAM_NOME, stdin);
+    strtok(contatos[*total].nome, "\n");  
+    printf("Telefone: ");
+    fgets(contatos[*total].telefone, TAM_NOME, stdin);
+    strtok(contatos[*total].telefone, "\n"); 
+
+    (*total)++;
+    printf("Contato adicionado com sucesso!\n");
+    getchar(); 
 }
 
-void listarContatos() {
-    printf("Lista de Contatos:\n");
-    for (int i = 0; i < totalContatos; i++) {
-        printf("%d. %s\n", i + 1, listaDeContatos[i]);
+void listarContatos(const Contato contatos[], int total) {
+    limparTela();
+    printf("Listar Contatos\n");
+    for (int i = 0; i < total; i++) {
+        printf("Contato %d:\n", i + 1);
+        printf("Nome: %s\n", contatos[i].nome);
+        printf("Telefone: %s\n", contatos[i].telefone);
+        printf("\n");
     }
+    getchar(); 
 }
 
-void removerContato() {
+void removerContato(Contato contatos[], int *total) {
     int indice;
-    listarContatos();
-    printf("Digite o número do contato que deseja remover: ");
-    scanf("%d", &indece);
+    limparTela();
+    printf("Remover Contato\n");
+    listarContatos(contatos, *total);
+    printf("Digite o número do contato a ser removido: ");
+    scanf("%d", &indice);
+    getchar(); 
 
-    if(indice > 0 &&indice <=totalContatos) {
-        for (int i = indice -1; i < totalContatos - 1; i++) {
-            strcpy(listaDeContatos[i], listadeContatos[i +1]);
-            }
-        totalContatos--;
-        printf("Contato removido com sucesso!\n");
-        } else {
-            printf("Índice inválido!\n")        
-        }
+    if (indice < 1 || indice > *total) {
+        printf("Contato inválido!\n");
+        return;
     }
+
+    for (int i = indice - 1; i < *total - 1; i++) {
+        contatos[i] = contatos[i + 1];
+    }
+
+    (*total)--;
+    printf("Contato removido com sucesso!\n");
+    getchar(); 
+}
