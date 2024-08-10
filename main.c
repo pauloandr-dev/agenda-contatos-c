56f5857642a80bf2c3ea99ca279ee18249be8a02
Pauloandr_ <117040493+pauloandr-dev@users.noreply.github.com>
2024-08-10 19:12:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "contatos.h"
 
-int main() {
+void menuPrincipal(Contato contatos[], int *total) {
     int opcao;
-    do {
-        printf("\nAgenda de Contatos\n");
+
+    while (1) {
+        limparTela();
+        printf("Agenda de Contatos\n");
         printf("1. Adicionar Contato\n");
         printf("2. Listar Contatos\n");
         printf("3. Remover Contato\n");
         printf("4. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
+        getchar(); 
 
         switch(opcao) {
             case 1:
-                adicionarContato();
+                adicionarContato(contatos, total);
                 break;
             case 2:
-                listarContatos();
+                listarContatos(contatos, *total);
                 break;
             case 3:
-                removerContato();
+                removerContato(contatos, total);
                 break;
             case 4:
+                limparTela();
                 printf("Saindo...\n");
-                break;
+                exit(0);
             default:
-                printf("Opcao invalida!\n");
+                printf("Opcao invalida! Tente novamente.\n");
+                getchar();
+                break;
         }
-    } while(opcao != 4);
+    }
+}
 
+int main() {
+    Contato contatos[MAX_CONTATOS];
+    int total = 0;
+
+    menuPrincipal(contatos, &total);
     return 0;
 }
